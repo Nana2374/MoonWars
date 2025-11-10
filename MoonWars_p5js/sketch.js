8292c388f5f4af9addc35f65575d9f17e82ca2d1
Sze-Ching <oszeching07@gmail.com>
2025-11-10 23:17:57 +0800

    Revert "fixed captcha problem"
    This reverts commit e19b4dd35d329f6bbc663fcb2da77146642bbe1b.

--- MoonWars_p5js/sketch.js
+++ MoonWars_p5js/sketch.js
@@ -523,8 +523,8 @@ function addCourseToSelection(course) {
 
 class CaptchaModal {
   constructor() {
-    this.w = 600;
-    this.h = 300;
+    this.w = 420;
+    this.h = 200;
     this.reset();
   }
   reset() {
